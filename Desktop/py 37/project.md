72380ee3a26b9c317cf24358393ea97d52c1b42e
Ivan Prokopev <ivan-rtk@mail.ru>
2022-11-22 11:02:58 +0300

    /*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом*/

---
+++ Desktop/py 37/project.md
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta htp-eqviv="X-UA-Compatible" content="ie=edge">
+	<title>
+</head>
+<body>
+    demo
+</body>
+</html>
---
+++ Desktop/обучение/тестировщик/C#-2/task4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом*/
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+System.Console.WriteLine("Ответ при решении первым способом:");
+if (IsPalindrome()) System.Console.WriteLine($"1. Введный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
